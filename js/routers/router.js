4f9a7d0fb9d51cbd73ae4ac79da8a9206f71811a
Mohd Ejaz Siddiqui <mohdejazsiddiqui@gmail.com>
2017-09-09 21:09:53 +0530

    Add files via upload

--- js/routers/router.js
+++ js/routers/router.js
@@ -1,1 +1,16 @@
 
+var Workspace = Backbone.Router.extend({
+	routes : {
+		'*filter' : 'setFilter'
+	},
+
+	setFilter : function(param) {
+		// set the currect filter to be used
+		// trigger a collection filer event, causing  hiding and unhiding
+		// of Todo view items
+		window.app.Todos.trigger('filter');
+	}
+});
+
+app.TodoRouter = new Workspace();
+Backbone.history.start();
