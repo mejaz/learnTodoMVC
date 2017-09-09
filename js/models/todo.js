f76effcf8309443f9bdb36f1b735e1c1c7469f73
Mohd Ejaz Siddiqui <mohdejazsiddiqui@gmail.com>
2017-09-09 21:09:03 +0530

    Add files via upload

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,1 +1,24 @@
 
+
+var app = app || {};
+
+// Todo Model
+//------------
+// this model has 'title', 'order' and 'completed' attributes
+
+
+app.Todo = Backbone.Model.extend({
+
+	defaults: {
+		title: '',
+		completed: false
+	},
+
+	// toggle the completed state of the todo item
+	toggle: function() {
+		this.save({
+			completed: !this.get('completed')
+		});
+	}
+
+});
